1d429d17bf103a714fb5d6046b4cdb586706cb3a
cookieidea <jhbhyvv@outlook.com>
2026-09-03 06:18:11 +0800

    fix: lsmash sourceRoot

--- pkgs/vs-plugins/vapoursynth-lsmash.nix
+++ pkgs/vs-plugins/vapoursynth-lsmash.nix
@@ -14,30 +14,15 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkg-config which ];
   buildInputs = [ vapoursynth ffmpeg_6 ];
 
+  sourceRoot = "source/VapourSynth";
   hardeningDisable = [ "all" ];
 
-  # 两阶段：根目录编 liblsmash → VapourSynth 插件
-  buildPhase = ''
-    runHook preBuild
-    # 1) lsmash core
-    ./configure --prefix=$TMPDIR/lsmash
-    make -j$NIX_BUILD_CORES -C lsmash
-    make -C lsmash install
-    # 2) VapourSynth 插件
-    cd VapourSynth
-    ./configure --prefix=$out --extra-cflags="-I$TMPDIR/lsmash/include" --extra-ldflags="-L$TMPDIR/lsmash/lib"
-    make -j$NIX_BUILD_CORES
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    make install
-    runHook postInstall
+  preConfigure = ''
+    patchShebangs configure
   '';
 
   meta = with lib; {
-    description = "L-SMASH Works VapourSynth plugin (lsmas)";
+    description = "L-SMASH Works VapourSynth plugin (lsmas, libav backend)";
     homepage = "https://github.com/VFR-maniac/L-SMASH-Works";
     license = licenses.isc;
     platforms = platforms.linux;
