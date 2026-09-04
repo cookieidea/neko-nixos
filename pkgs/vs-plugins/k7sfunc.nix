6a1ac5f4e22821386ad105d2dd99f209413a15f7
cookieidea <jhbhyvv@outlook.com>
2026-09-04 14:15:58 +0800

    fix: 初始化 k7sfunc RIFE_STD skip

--- pkgs/vs-plugins/k7sfunc.nix
+++ pkgs/vs-plugins/k7sfunc.nix
@@ -16,6 +16,12 @@ buildPythonPackage rec {
 
   doCheck = false;
 
+  postPatch = ''
+    # k7sfunc 1.8.1：RIFE_STD 在设置 skip 前读取它，导致 UnboundLocalError。
+    substituteInPlace k7sfunc/mod_memc.py \
+      --replace-fail $'\t_check_plugin(func_name, "rife")\n\tif skip :' $'\t_check_plugin(func_name, "rife")\n\tskip = turbo == 2\n\tif skip :'
+  '';
+
   meta = with lib; {
     description = "VapourSynth video processing functions optimized for media players";
     homepage = "https://github.com/hooke007/K7sfunc";
