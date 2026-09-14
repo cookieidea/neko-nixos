e9aba4e9882d8f625ddffc4e78c469df35a879e1
cookieidea <jhbhyvv@outlook.com>
2026-09-14 18:34:11 +0800

    feat: upload 配置声明式管理（freeimage.host）

--- dotfiles/config/mark-shot/inject-secrets.py
+++ dotfiles/config/mark-shot/inject-secrets.py
@@ -4,4 +4,16 @@ config = json.load(open("/home/cookie/.config/mark-shot/config.json"))
 config.setdefault("translation", {}).setdefault("youdao", {})["appKey"] = secret.get("youdaoAppKey", "")
 config.setdefault("translation", {}).setdefault("youdao", {})["appSecret"] = secret.get("youdaoAppSecret", "")
 config.setdefault("upload", {}).setdefault("env", {})["MARK_SHOT_UPLOAD_FIELD_key"] = secret.get("freeimageKey", "")
+# 非敏感 upload 配置
+config["upload"] = {
+    "command": "",
+    "env": {
+        "MARK_SHOT_UPLOAD_URL": "https://freeimage.host/api/1/upload",
+        "MARK_SHOT_UPLOAD_FIELD": "source",
+        "MARK_SHOT_UPLOAD_URL_PATH": "image.url",
+        "MARK_SHOT_UPLOAD_DELETE_URL_PATH": "",
+        "MARK_SHOT_UPLOAD_FIELD_key": secret.get("freeimageKey", "")
+    },
+    "timeoutMs": 60000
+}
 json.dump(config, open("/home/cookie/.config/mark-shot/config.json", "w"), indent=4, ensure_ascii=False)
