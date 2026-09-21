5520338f491d886e8bbcddcb92a3472298996ca7
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 04:08:20 +0800

    refactor(layout): update flattened configuration paths

--- flake.nix
+++ flake.nix
@@ -108,7 +108,7 @@
 
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.users.${username} = import ./configuration/ATRI/home.nix;
+        home-manager.users.${username} = import ./configuration/home.nix;
         home-manager.extraSpecialArgs = { inherit desktop username cooknixvim bilihud selfPackages noctalia bestclient mark-shot llm-agents-nix hmLib; };
       };
     in {
@@ -116,12 +116,12 @@
       packages.${system} = selfPackages;
 
       nixosConfigurations = {
-        # 实体机；硬件配置见 configuration/device/hardware/hardware-config.nix（需 git add）
+        # 实体机；硬件配置见 configuration/device/hardware-config.nix（需 git add）
         ${hostname} = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit noctalia-greeter; };
           modules = [
-            ./configuration/ATRI/system.nix
+            ./configuration/system.nix
             hmModule
             agenix.nixosModules.default
             # overlays 见 configuration/overlays/default.nix
