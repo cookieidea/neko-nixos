94640c403e3604f2c694c5eecf1459e75aebbfd6
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 04:08:13 +0800

    refactor(layout): fix service import paths

--- configuration/modules.nix
+++ configuration/modules.nix
@@ -9,13 +9,13 @@
     ./modules/dsh.nix          # dsh web 端口
 
     # --- 系统服务 ---
-    ../modules/services/openssh.nix
-    ../modules/services/udisks2.nix
-    ../modules/services/gvfs.nix
-    ../modules/services/snapper.nix
-    ../modules/services/sunshine.nix
-    ../modules/services/lact-smartd.nix
-    ../modules/services/kdeconnect.nix   # KDE Connect 端口范围
+    ./modules/services/openssh.nix
+    ./modules/services/udisks2.nix
+    ./modules/services/gvfs.nix
+    ./modules/services/snapper.nix
+    ./modules/services/sunshine.nix
+    ./modules/services/lact-smartd.nix
+    ./modules/services/kdeconnect.nix   # KDE Connect 端口范围
 
     # --- 虚拟化 / 游戏运行时 ---
     ./modules/virtualisation.nix
