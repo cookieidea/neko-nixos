0249b7338558dd31cbd5890fe5b7db50a29698be
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 04:10:49 +0800

    refactor(layout): fix Home Manager import comments

--- configuration/home.nix
+++ configuration/home.nix
@@ -8,9 +8,9 @@
 
     ./home/session          # 会话变量 + systemd user 服务
     ./home/packages         # home.packages（按用途分类）
-    ./home/programs.nix# HM 程序选项（git/fish/noctalia…）
-    ./home/desktop.nix# 桌面（polkit/GTK/KDE Connect/OBS）
-    ./home/xdg.nix# xdg.configFile / xdg.dataFile
+    ./home/programs.nix # HM 程序选项（git/fish/noctalia…）
+    ./home/desktop.nix  # 桌面（polkit/GTK/KDE Connect/OBS）
+    ./home/xdg.nix      # xdg.configFile / xdg.dataFile
     ./home/files            # home.file + activation
   ];
 }
