5070e3f8ebb1574c5ef35007cde23fe15ddc575e
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 05:38:10 +0800

    refactor: simplify comments in configuration/pkgs/file-managers/nautilus-extensions/nautilus-with-extensions.nix

--- configuration/pkgs/file-managers/nautilus-extensions/nautilus-with-extensions.nix
+++ configuration/pkgs/file-managers/nautilus-extensions/nautilus-with-extensions.nix
@@ -1,4 +1,4 @@
-# 带扩展的 nautilus（symlinkJoin 合入 extensions-4 目录）
+# 合并扩展后的 Nautilus。
 { pkgs }:
 let
   imgconv = import ./image-converter.nix { inherit pkgs; };
@@ -8,10 +8,10 @@ pkgs.symlinkJoin {
   paths = [ pkgs.nautilus ];
   postBuild = ''
     mkdir -p "$out/lib/nautilus/extensions-4"
-    # image-converter（C 扩展）
+    # image-converter C 扩展。
     ln -s "${imgconv}/lib/nautilus/extensions-4/libnautilus-image-converter.so" \
       "$out/lib/nautilus/extensions-4/"
-    # nautilus-python（Python 扩展加载器，扫描 ~/.local/share/nautilus-python/extensions）
+    # nautilus-python 扩展加载器。
     ln -s "${pkgs.nautilus-python}/lib/nautilus/extensions-4/libnautilus-python.so" \
       "$out/lib/nautilus/extensions-4/"
   '';
