a0e917e2eefd1a553c2f501d85b5778dec6fb4fc
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 04:38:40 +0800

    refactor: 拆分 Desktop 软件包

---
+++ configuration/home/packages/Desktop/files.nix
@@ -0,0 +1,24 @@
+{ pkgs, selfPackages, hmLib, ... }:
+
+{
+  home.packages = with pkgs; [
+    (pkgs.symlinkJoin {
+      name = "nautilus-wrapper";
+      paths = [ pkgs.nautilus ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/nautilus           --set NAUTILUS_4_EXTENSION_DIR "${selfPackages.nautilus-with-extensions}/lib/nautilus/extensions-4"           --prefix PATH : "${pkgs.lib.makeBinPath [ pkgs.imagemagick pkgs.jpegoptim pkgs.pngquant pkgs.ffmpeg pkgs.coreutils ]}"
+      '';
+    })
+    nautilus-python
+    localsearch
+    gvfs
+    ffmpegthumbnailer
+    file-roller
+    webp-pixbuf-loader
+    poppler
+    jpegoptim
+    pngquant
+    hmLib.xdgOpenWithGio
+  ];
+}
