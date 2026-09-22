ac8f50a8b3c9562d15080765898fe216055195f0
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 18:26:32 +0000

    refactor: use wrapped nautilus package directly

--- configuration/home/packages/Desktop/files.nix
+++ configuration/home/packages/Desktop/files.nix
@@ -2,22 +2,8 @@
 
 {
   home.packages = with pkgs; [
-    (pkgs.symlinkJoin {
-      name = "nautilus-wrapper";
-      paths = [ pkgs.nautilus ];
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        wrapProgram "$out/bin/nautilus" \
-          --set NAUTILUS_4_EXTENSION_DIR "${selfPackages.nautilus-with-extensions}/lib/nautilus/extensions-4" \
-          --prefix PATH : "${pkgs.lib.makeBinPath [
-            pkgs.imagemagick
-            pkgs.jpegoptim
-            pkgs.pngquant
-            pkgs.ffmpeg
-            pkgs.coreutils
-          ]}"
-      '';
-    })
+    # Nautilus 的扩展、PATH 和运行时变量由自构建包统一处理。
+    selfPackages.nautilus-with-extensions
     nautilus-python
     localsearch
     gvfs
