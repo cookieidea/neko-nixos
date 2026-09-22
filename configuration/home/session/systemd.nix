b4d26f884d2e316d80726a3cf6a9691e91c2d7cf
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 18:27:47 +0000

    refactor: remove global nautilus runtime environment

--- configuration/home/session/systemd.nix
+++ configuration/home/session/systemd.nix
@@ -2,9 +2,8 @@
 
 {
   systemd.user.sessionVariables = hmLib.devEnv // {
-    # Nautilus 扩展。
+    # Nautilus 扩展环境由 nautilus-with-extensions 自身的 wrapper 提供。
     GI_TYPELIB_PATH = "${pkgs.nautilus}/lib/girepository-1.0";
-    NAUTILUS_4_EXTENSION_DIR = hmLib.nautilusExtensionDir;
 
     # XDG_DATA_DIRS 由系统环境提供，不在此覆盖。
     # X11/XWayland 应用需要 GTK/Qt 输入法模块。
