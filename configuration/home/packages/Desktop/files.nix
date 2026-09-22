c0bdf4a25cdad292b373e3efca1353354bd36d3a
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 19:09:30 +0000

    remove redundant nautilus-python package

--- configuration/home/packages/Desktop/files.nix
+++ configuration/home/packages/Desktop/files.nix
@@ -4,7 +4,6 @@
   home.packages = with pkgs; [
     # Nautilus 的扩展、PATH 和运行时变量由自构建包统一处理。
     selfPackages.nautilus-with-extensions
-    nautilus-python
     localsearch
     gvfs
     ffmpegthumbnailer
