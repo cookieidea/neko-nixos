db802456c7f65dfeabab62d2caa278cd56092cda
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-22 19:10:17 +0000

    scope Nautilus GI typelib environment

--- configuration/home/session/systemd.nix
+++ configuration/home/session/systemd.nix
@@ -2,9 +2,6 @@
 
 {
   systemd.user.sessionVariables = hmLib.devEnv // {
-    # Nautilus 扩展环境由 nautilus-with-extensions 自身的 wrapper 提供。
-    GI_TYPELIB_PATH = "${pkgs.nautilus}/lib/girepository-1.0";
-
     # XDG_DATA_DIRS 由系统环境提供，不在此覆盖。
     # X11/XWayland 应用需要 GTK/Qt 输入法模块。
     GTK_IM_MODULE = "fcitx";
