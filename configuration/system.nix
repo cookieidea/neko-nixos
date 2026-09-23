10e7c49e6d6a961b99e5b5d20d1ac464ec969c53
CookieIdea <124411914+cookieidea@users.noreply.github.com>
2026-09-23 13:14:58 +0000

    chore: 精简代码注释

--- configuration/system.nix
+++ configuration/system.nix
@@ -1,9 +1,7 @@
-# 系统配置入口：聚合 system/ + device/ + modules/
 { ... }:
 
 {
   imports = [
-    # --- 纯系统级配置 ---
     ./system/nix.nix
     ./system/boot.nix
     ./system/networking.nix
@@ -11,12 +9,8 @@
     ./system/audio-bluetooth.nix
     ./system/security-users.nix
     ./system/secrets.nix
-    ./system/packages.nix        # 系统级工具包（全局可见）
-
-    # --- 硬件特定 ---
+    ./system/packages.nix
     ./device.nix
-
-    # --- 程序 / 服务 / 虚拟化 模块 ---
     ./modules.nix
   ];
 }
